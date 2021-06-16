87992863f7f68732a413a288f90837b8662b289c
Rosa Pavón <mrosapavon@gmail.com>
2021-06-16 17:23:23 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 
-function App() {
+function Nombres() {
+  const dcvsfondo=[
+   "BOB, ",
+    "PATRICIO, ",
+    "ARENITA, ",
+    "CALAMARDO, ",
+    "BRUCE, ",
+    "CLACK, ", 
+    "DIANA, ",
+    "BARRY, ",
+    "OLIVER.",
+   
+  ];
+
+  const personajillos = (i) => {
+    return (
+      <>
+        {dcvsfondo[i]}
+        </>
+      
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {personajillos(0)}
+        {personajillos(1)}
+        {personajillos(2)}
+        {personajillos(3)}
+        {personajillos(4)}
+        {personajillos(5)}
+        {personajillos(6)}
+        {personajillos(7)}
+        {personajillos(8)}
+      </div>
+    </>
   );
 }
+
+
+
+function Cabecera() {
+  return (
+    <header>
+      <h1>Rutas en React</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/inicio">Nombres</Link>
+   
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function App() {
+
+
+  return (
+      <BrowserRouter>
+          <Cabecera />
+              <Route path="/inicio">
+              <Nombres />
+          </Route>
+      </BrowserRouter>
+  );
+}
+
 
 export default App;
